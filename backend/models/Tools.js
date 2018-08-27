c644d38e87daf07af1a0a4e8484ed02650f77df1
Adhiraj Tikku <adhiraj.192@gmail.com>
2018-08-27 12:26:28 -0700

    updating tool model schema

--- backend/models/Tools.js
+++ backend/models/Tools.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose');
 
 var Schema = new mongoose.Schema({
+  rank: Number,
   name: String,
   url: String,
-  imgurl: String
+  icon: String,
+  description: String,
 });
 
 var Tool = mongoose.model('Tools', Schema);
